756ab737e2749f11969252b888f9fce9bacb6dff
Mawuli-Kplim Asase <masase@c1r7s3.42lausanne.ch>
2024-12-06 17:00:55 +0100

    gaz bombe

--- main.c
+++ main.c
@@ -6,16 +6,41 @@
 /*   By: masase <masase@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 10:08:43 by maw               #+#    #+#             */
-/*   Updated: 2024/12/05 18:24:53 by masase           ###   ########.fr       */
+/*   Updated: 2024/12/06 17:00:21 by masase           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
 	if (ac != 4)
 		return (0);
 	
 
 }
+
+// #include <sys/types.h>
+// #include <unistd.h>
+// #include <stdio.h>
+
+// int main() {
+//     pid_t pid;
+
+//     pid = fork(); // Fork creates a new process
+
+//     if (pid == -1) {
+//         perror("fork failed");
+//         return 1;
+//     }
+
+//     if (pid == 0) {
+//         // Child process
+//         printf("I am the child process. My PID is %d\n", getpid());
+//     } else {
+//         // Parent process
+//         printf("I am the parent process. My PID is %d, and my child's PID is %d\n", getpid(), pid);
+//     }
+
+//     return 0;
+// }
--- pipex.h
+++ pipex.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maw <maw@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: masase <masase@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 15:52:20 by maw               #+#    #+#             */
-/*   Updated: 2024/12/05 16:39:32 by maw              ###   ########.fr       */
+/*   Updated: 2024/12/06 16:47:14 by masase           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 # define PIPEX_H
 # include "LIBFT/libft.h"
 # include <fcntl.h>
+
+typedef struct s_pipex
+{
+	int		in_fd;
+	int		out_fd;
+	char	**cmd_path;
+	char	**cmd_arg;
+	int		cmd_count;
+}	t_pipex;
+
 #endif
 
